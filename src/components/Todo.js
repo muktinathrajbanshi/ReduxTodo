2fcc182556e306dea735c6cf603a5411c4da4bbc
unknown <muktinathrajbanshi786@gmail.com>
2025-07-15 15:03:35 +0545

    50% work done

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {useDispatch} from "react-redux";
+import {addTodo} from "../actions/index";
 import "./todo.css";
 
+
 const Todo = () => {
+
+    const [inputData, setInputData] = useState("");
+    const dispatch = useDispatch();
+
   return (
     <>
       <div className="main-div">
@@ -9,12 +16,13 @@ const Todo = () => {
         <figure>
           <figcaption> Add Your List Here ✌️</figcaption>
         </figure>
-
           <div className="addItems">
-            <input type="text" placeholder="✍️ Add Items.." />
-            <i class="fa-solid fa-plus"></i>
+            <input type="text" placeholder="✍️ Add Items.."
+            value={inputData} 
+            onChange={(event) => setInputData(event.target.value)}
+             />
+            <i className="fa-solid fa-plus" onClick={() =>dispatch(addTodo(inputData), setInputData(""))}></i>
           </div>
-
         </div>
       </div>
     </>
